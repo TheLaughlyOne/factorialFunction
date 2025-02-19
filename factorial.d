0d1bca547a263f7454078fa03116607c9c444a0c
Pol <returnbacktomonke@gmail.com>
2025-02-18 20:23:16 -0700

    Comments added

--- factorial.d
+++ factorial.d
@@ -14,11 +14,13 @@ void get()
     {
         writeln("Type any integer");
         input = readln().replace("\n", "");
+        // as the user inputs their integer, the terminating character is replaced with nothing, reducing it to 0
 
+        // converts the string user input into a long, and then making what is to be factored equal to it
         factor = to!long(input);
         factorBuffer = factor;
 
-        break;
+        break; // exits the loop
     }
 }
 
@@ -29,24 +31,26 @@ void operation()
 
     for(int i=0; i<factorBuffer; i++)
     {
-        if(i == 0)
+        // anything multiplied by zero is equal to zero, therefore when i=0, it will continue to the next iteration
+        if(i == 0) 
         {
             continue;
         }
         else
         {
-            productBuffer = factor;
-            productBuffer = i*factor;
-            factor = i*factor;
+            productBuffer = factor; // the product buffer is equal to the factor
+            productBuffer = i*factor; // then it becomes equal to i multiplied to the factor
+            factor = i*factor; // factor also becomes equal to i multiplied by itself
         }
     }
 
-    product = productBuffer;
-    writeln(product, "\n");
+    product = productBuffer; // the product is equal to the buffer
+    writeln(product, "\n"); // then it outputs the product
 }
 
 void repetition()
 {
+    // repeats every function, including itself
     get();
     operation();
     repetition();
@@ -54,9 +58,9 @@ void repetition()
 
 int main()
 {
-    get();
-    operation();
-    repetition();
+    get(); // get function occurs first
+    operation(); // the operation occurs next
+    repetition(); // then it will loop in repetition for as long as the application is openned
 
-    return 0;
+    return 0; // returns 0, signifying it has ran successfully
 }
